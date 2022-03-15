19830fff5397c549dfebc816ab565ae04b0f826d
Daniel Santos <daniel.san.brito@live.com>
2022-03-15 09:07:16 -0300

    truthy and falsy

---
+++ Fundamentos  com JavaScript/truthy_falsy.js
@@ -0,0 +1,32 @@
+//boolean
+
+// const usuarioLogado = true
+// const contaPaga = false
+
+// truthy ou falsy
+
+// o =  false
+// 1 = true 
+//' ' = false string vazia e falsa 
+// null  = Objeto
+// undefined = vazio ou nada
+
+
+//  console.log(0 == false);
+//  console.log("" == false);
+//  console.log(1 == false);
+//  console.log(null == false)
+//  console.log(undefined == false)
+
+
+ let minhaVar;
+ let varNull = null;
+
+ console.log(minhaVar);
+ console.log(varNull);
+
+let numero = 3 ;
+let texro = "Alura";
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
