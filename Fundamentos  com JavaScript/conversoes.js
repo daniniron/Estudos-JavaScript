0a383652cba0121898008e59d90c21e406f37e3f
Daniel Santos <daniel.san.brito@live.com>
2022-03-15 14:06:45 -0300

    estudos  de comparação, conversão, templates e ternario

--- Fundamentos  com JavaScript/conversoes.js
+++ Fundamentos  com JavaScript/conversoes.js
@@ -3,10 +3,10 @@
 
 // conversao implicita
 
-const numero = 456;
-const numeroString = "456"
-console.log(numero == numeroString) //Diferenças entre ===, == 
-console.log( numero + Number(numeroString))
+// const numero = 456;
+// const numeroString = "456"
+// console.log(numero == numeroString) //Diferenças entre ===, == 
+// console.log( numero + Number(numeroString))
 
 // conversao explicita 
 
